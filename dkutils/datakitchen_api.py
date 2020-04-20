0fb6904eba3be658716239336b4918063114f9cf
ddicara <ddicara@datakitchen.io>
2020-04-20 13:44:13 -0400

    Add create order function.

--- dkutils/datakitchen_api.py
+++ dkutils/datakitchen_api.py
@@ -6,7 +6,7 @@ DEFAULT_DATAKITCHEN_URL = 'https://cloud.datakitchen.io'
 
 def get_headers(username, password, datakitchen_url=DEFAULT_DATAKITCHEN_URL):
     """
-    Login to the DataKitchen platform via the login API to retreive a token. Store the \
+    Login to the DataKitchen platform via the login API to retrieve a token. Store the \
     token in headers required to make subsequent API requests.
 
     Parameters
@@ -15,8 +15,8 @@ def get_headers(username, password, datakitchen_url=DEFAULT_DATAKITCHEN_URL):
       DataKitchen platform username.
     password : str
       DataKitchen platform password.
-    datakitchen_url : str
-      DataKitchen platform url.
+    datakitchen_url : str, optional
+        DataKitchen platform URL (default is https://cloud.datakitchen.io)
 
     Raises
     ------
@@ -26,7 +26,7 @@ def get_headers(username, password, datakitchen_url=DEFAULT_DATAKITCHEN_URL):
     Returns
     -------
     dict
-        Dictionary required by subsequent API requests
+        Dictionary of headers required by subsequent API requests
     """
     credentials = dict()
     credentials['username'] = username
@@ -37,3 +37,40 @@ def get_headers(username, password, datakitchen_url=DEFAULT_DATAKITCHEN_URL):
     response.raise_for_status()
     return {'Authorization': f'Bearer {response.text}'}
 
+
+def create_order(headers, kitchen, recipe, variation, parameters={}, datakitchen_url=DEFAULT_DATAKITCHEN_URL):
+    """
+
+    Parameters
+    ----------
+    headers : dict
+        Headers containing authentication token.
+    kitchen :  str
+        Kitchen name
+    recipe : str
+        Recipe name
+    variation : str
+        Variation name
+    parameters : dict, optional
+        Dictionary of variable overrides (default is empty dictionary)
+    datakitchen_url : str, optional
+        DataKitchen platform URL (default is https://cloud.datakitchen.io)
+
+    Raises
+    ------
+    HTTPError
+        If the request fails.
+
+    Returns
+    ------
+    requests.Response
+        :class:`Response <Response>` object
+    """
+    order_create_url = f'{datakitchen_url}/v2/order/create/{kitchen}/{recipe}/{variation}'
+    payload = {
+        "schedule": "now",
+        "parameters": parameters
+    }
+    response = requests.put(order_create_url, headers=headers, json=payload)
+    response.raise_for_status()
+    return response
--- tests/test_datakitchen_api.py
+++ tests/test_datakitchen_api.py
@@ -1,12 +1,37 @@
 from unittest import TestCase
+from unittest.mock import patch
 
 from requests.exceptions import HTTPError
 
-from dkutils.datakitchen_api import get_headers
+from dkutils.datakitchen_api import get_headers, create_order
+
+DUMMY_AUTH_TOKEN = 'DATAKITCHEN_TOKEN'
+DUMMY_HEADERS = {'Authorization': f'Bearer {DUMMY_AUTH_TOKEN}'}
 
 
 class TestDatakitchenAPI(TestCase):
 
-    def test_get_headers(self):
+    @patch('dkutils.datakitchen_api.requests.post')
+    def test_get_headers(self, mock_post):
+        mock_post.return_value.text = DUMMY_AUTH_TOKEN
+        headers = get_headers('Foo', 'Bar')
+        self.assertEqual(headers, DUMMY_HEADERS)
+
+    def test_get_headers_exception(self):
         with self.assertRaises(HTTPError):
             get_headers('Foo', 'Bar')
+
+    @patch('dkutils.datakitchen_api.requests.put')
+    def test_create_order(self, mock_put):
+        response_json = {
+            "order_id": "abd8c538-705d-11ea-99d3-2699c9f5d2a0",
+            "variable_overrides": {},
+            "status": "success"
+        }
+        mock_put.return_value.json = lambda: response_json
+        response = create_order(DUMMY_HEADERS, 'kitchen', 'recipe', 'variation')
+        self.assertEqual(response.json(), response_json)
+
+    def test_create_order_exception(self):
+        with self.assertRaises(HTTPError):
+            create_order(DUMMY_HEADERS, 'kitchen', 'recipe', 'variation')
