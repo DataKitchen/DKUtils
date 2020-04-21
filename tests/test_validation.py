42847e8527cb5a99eca980ccb75192f08bd91adb
ddicara <ddicara@datakitchen.io>
2020-04-21 13:10:26 -0400

    Bugfix global variable validation.

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -6,15 +6,15 @@ from dkutils.validation import validate_globals
 class TestValidation(TestCase):
 
     def test_valid_global(self):
-        validate_globals(['__name__'])
+        validate_globals(['__name__'], globals())
 
     def test_valid_globals(self):
-        validate_globals(['__name__', '__file__'])
+        validate_globals(['__name__', '__file__'], globals())
 
     def test_invalid_global(self):
         with self.assertRaises(NameError):
-            validate_globals(['Foo'])
+            validate_globals(['Foo'], globals())
 
     def test_invalid_globals(self):
         with self.assertRaises(NameError):
-            validate_globals(['Foo', 'Bar', '__name__'])
+            validate_globals(['Foo', 'Bar', '__name__'], globals())
