f7e001e84987f2fd2e44f02fe362e3f568b031d4
ddicara <ddicara@datakitchen.io>
2020-04-22 16:07:55 -0400

    Updates based on PR feedback

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -5,16 +5,29 @@ from dkutils.validation import validate_globals
 
 class TestValidation(TestCase):
 
-    def test_valid_global(self):
-        validate_globals(['__name__'], globals())
-
     def test_valid_globals(self):
-        validate_globals(['__name__', '__file__'], globals())
-
-    def test_invalid_global(self):
-        with self.assertRaises(NameError):
-            validate_globals(['Foo'], globals())
+        validate_globals(['__name__', '__file__'])
 
     def test_invalid_globals(self):
         with self.assertRaises(NameError):
-            validate_globals(['Foo', 'Bar', '__name__'], globals())
+            validate_globals(['Foo', 'Bar', '__name__'])
+
+    def test_zero_valid(self):
+        global zero
+        zero = 0
+        validate_globals(['zero'])
+
+    def test_empty_string_valid(self):
+        global empty_string
+        empty_string = ''
+        validate_globals(['empty_string'])
+
+    def test_none_valid(self):
+        global none
+        none = None
+        validate_globals(['none'])
+
+    def test_undefined_invalid(self):
+        global undefined
+        with self.assertRaises(NameError):
+            validate_globals(['undefined'])
