342463100d106607ebf47c5c50ad9f5217562a33
DataKitchen <prod@datakitchen.io>
2020-04-22 21:50:09 +0000

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="DKUtils",
-    version="0.0.2",
+    version="0.0.3",
     author="DataKitchen",
     author_email="info@datakitchen.io",
     description="DataKitchen Utils Library",
