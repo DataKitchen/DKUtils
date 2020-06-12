c7f2c9edec4e1fae13c52cd835ef47d7a57c295c
DataKitchen <prod@datakitchen.io>
2020-06-12 15:12:44 +0000

    Bump version: 0.7.0 → 0.7.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="DKUtils",
-    version="0.7.0",
+    version="0.7.1",
     author="DataKitchen",
     author_email="info@datakitchen.io",
     description="DataKitchen Utils Library",
