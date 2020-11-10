ed419013ab671c9e8ef26ced36c7555ce885c1eb
DataKitchen <prod@datakitchen.io>
2020-11-10 17:58:15 +0000

    Bump version: 1.0.0 → 1.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2020, DataKitchen'
 author = 'DataKitchen'
 
 # The full version, including alpha/beta/rc tags
-release = u'1.0.0'
+release = u'1.0.1'
 
 # -- General configuration ---------------------------------------------------
 
