f06089dc6da090c717cf177499cab574f96679fa
ernest <ehill@datakitchen.io>
2021-10-20 18:05:38 +0300

    Initial version

---
+++ dkutils/smtp_api/sender.py
@@ -0,0 +1,97 @@
+import mimetypes
+from smtplib import SMTP
+from email.message import EmailMessage
+from email.utils import COMMASPACE
+
+
+def _convert_to_strings(list_of_strs):
+    return (
+        COMMASPACE.join(list_of_strs) if isinstance(list_of_strs, (list, tuple)) else list_of_strs
+    )
+
+
+def create_message(
+    sender, recipients, subject, plain_text, html_text=None, files=None, reply_addresses=None
+):
+    """
+        Create a message for an email.
+
+        Parameters
+        ----------
+        sender : str
+            Email address of the sender.
+        recipients : str|list
+            Email address(es) of the receiver.
+        subject : str
+            The subject of the email message.
+        plain_text : str
+            The plain text of the email message.
+        html_text: str
+            The html text of the email message.
+        files : str|list
+            The path(s) of the file(s) to be attached.
+        reply_addresses: str|list
+            Email address(es) replies should be sent to
+
+        Returns
+        -------
+        EmailMessage
+            An EmailMessage that can be sent
+        """
+    message = EmailMessage()
+    message["Subject"] = subject
+    message["From"] = sender
+    message["To"] = _convert_to_strings(recipients)
+    if reply_addresses:
+        message["Reply-To"] = _convert_to_strings(reply_addresses)
+    message.set_content(plain_text)
+    if html_text:
+        message.make_alternative()
+        message.add_attachment(html_text, 'html')
+    if files:
+        if isinstance(files, str):
+            files = [files]
+        for file_path in files:
+            content_type, encoding = mimetypes.guess_type(file_path)
+            if content_type is None or encoding is not None:
+                content_type = 'application/octet-stream'
+            maintype, subtype = content_type.split('/', 1)
+            with open(file_path, 'rb') as fp:
+                data = fp.read()
+            message.add_attachment(data, maintype=maintype, subtype=subtype)
+
+    return message
+
+
+class SMTP_Sender():
+    """
+        Class that can send emails using SMTP.
+
+        Parameters
+        ----------
+        host:str
+            The hostname of the SMTP server
+        user:str
+            The username to login to SMTP server
+        password: str
+            The password to be used to login to the SMTP server
+        port:int
+            The port of the SMTP server
+        use_tls: bool
+            A boolean indicating whether or not to use TLS in communications with the server
+        """
+
+    def __init__(self, host, user, password, port=587, use_tls=True):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.use_tls = use_tls
+
+    def do_send(self, message):
+        with SMTP(self.host, self.port) as server:
+            if self.use_tls:
+                server.starttls()
+            if self.user and self.password:
+                server.login(self.user, self.password)
+            server.send_message(message)
---
+++ tests/smtp_api/test_smtp_api_sender.py
@@ -0,0 +1,119 @@
+import tempfile
+from pathlib import Path
+from unittest import TestCase
+from unittest.mock import patch, call
+
+from dkutils.smtp_api.sender import create_message, SMTP_Sender
+
+SENDER = 'someone@somewhere.com'
+RECIPIENT = 'someone_else@somewhere.com'
+SUBJECT = 'Something Important'
+PLAIN_TEXT = "Some arbitrary message."
+HTML_TEXT = "<html><h1>Some Header</h1><p>test</p></html>"
+PASSWORD = "somesecret"
+HOST = "server.com"
+
+
+class Test(TestCase):
+
+    def setUp(self):
+        self.test_dir = tempfile.TemporaryDirectory()
+
+    def tearDown(self):
+        self.test_dir.cleanup()
+
+    def test_create_message_when_no_parameters(self):
+        with self.assertRaises(TypeError) as cm:
+            create_message()
+        self.assertEqual(
+            cm.exception.args[0],
+            "create_message() missing 4 required positional arguments: 'sender', "
+            "'recipients', 'subject', and 'plain_text'"
+        )
+
+    def test_create_message_plain_text(self):
+        message = create_message(SENDER, RECIPIENT, SUBJECT, PLAIN_TEXT)
+        expected_message = f'Subject: {SUBJECT}\n' \
+                           f'From: {SENDER}\n' \
+                           f'To: {RECIPIENT}\n' \
+                           'Content-Type: text/plain; charset="utf-8"\n' \
+                           'Content-Transfer-Encoding: 7bit\nMIME-Version: 1.0\n\n' \
+                           f'{PLAIN_TEXT}\n'
+        self.assertEqual(expected_message, message.as_string())
+
+    @patch('dkutils.smtp_api.sender.EmailMessage')
+    def test_create_message_html_text(self, _):
+        message = create_message(SENDER, RECIPIENT, SUBJECT, PLAIN_TEXT, HTML_TEXT)
+
+        message.__setitem__.assert_has_calls([
+            call('Subject', SUBJECT),
+            call('From', SENDER),
+            call('To', RECIPIENT)
+        ])
+        message.set_content.assert_called_once_with(PLAIN_TEXT)
+        message.make_alternative.assert_called_once()
+        message.add_attachment.assert_called_once_with(HTML_TEXT, 'html')
+
+    @patch('dkutils.smtp_api.sender.EmailMessage')
+    def test_create_message_with_attachment(self, _):
+        data = "some data"
+        file_name = Path(self.test_dir.name) / 'file.txt'
+        with file_name.open("w+") as output_file:
+            output_file.write(data)
+
+        message = create_message(SENDER, RECIPIENT, SUBJECT, PLAIN_TEXT, files=str(file_name))
+        message.__setitem__.assert_has_calls([
+            call('Subject', SUBJECT),
+            call('From', SENDER),
+            call('To', RECIPIENT)
+        ])
+        message.set_content.assert_called_once_with(PLAIN_TEXT)
+        message.add_attachment.assert_called_once_with(
+            str.encode(data), maintype='text', subtype='plain'
+        )
+
+    def test_constructor_with_defaults(self):
+        sender = SMTP_Sender(HOST, SENDER, PASSWORD)
+
+        self.assertEqual(sender.host, HOST)
+        self.assertEqual(sender.user, SENDER)
+        self.assertEqual(sender.password, PASSWORD)
+        self.assertEqual(sender.port, 587)
+        self.assertTrue(sender.use_tls)
+
+    def test_constructor(self):
+        port = 25
+
+        sender = SMTP_Sender(HOST, SENDER, PASSWORD, port, False)
+
+        self.assertEqual(sender.host, HOST)
+        self.assertEqual(sender.user, SENDER)
+        self.assertEqual(sender.password, PASSWORD)
+        self.assertEqual(sender.port, port)
+        self.assertFalse(sender.use_tls)
+
+    @patch('dkutils.smtp_api.sender.SMTP')
+    def test_do_send_with_tls(self, mock_smtp):
+        message = {}
+
+        SMTP_Sender(HOST, SENDER, PASSWORD).do_send(message)
+
+        mock_smtp.assert_called_once_with(HOST, 587)
+        mock_smtp.return_value.__enter__.assert_called_once()
+        mock_server = mock_smtp.return_value.__enter__.return_value
+        mock_server.starttls.assert_called_once()
+        mock_server.login.assert_called_once_with(SENDER, PASSWORD)
+        mock_server.send_message.assert_called_once_with(message)
+
+    @patch('dkutils.smtp_api.sender.SMTP')
+    def test_do_send_without_tls(self, mock_smtp):
+        message = {}
+
+        SMTP_Sender(HOST, user=None, password=None, port=25, use_tls=False).do_send(message)
+
+        mock_smtp.assert_called_once_with(HOST, 25)
+        mock_smtp.return_value.__enter__.assert_called_once()
+        mock_server = mock_smtp.return_value.__enter__.return_value
+        mock_server.starttls.assert_not_called()
+        mock_server.login.assert_not_called()
+        mock_server.send_message.assert_called_once_with(message)
