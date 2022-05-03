65f4ce95a134191a9e125cb74cffb7a82f145cf7
ddicara <ddicara@datakitchen.io>
2022-05-03 09:18:00 -0400

    Add get/add/delete/update staff methods to kitchen

--- dkutils/datakitchen_api/kitchen.py
+++ dkutils/datakitchen_api/kitchen.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
+import itertools
 import logging
 
 from requests import Response
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Union
 
 from dkutils.constants import (
     API_DELETE,
@@ -143,6 +144,111 @@ class Kitchen:
         )
         return response.json()
 
+    def _get_roles(self, settings: dict = None) -> dict:
+        """
+        Retrieve the staff and their associated roles assigned to this kitchen.
+
+        Parameters
+        ----------
+        settings : dict, optional
+            Dictionary representing kitchen_settings.json.
+
+        Raises
+        ------
+        HTTPError
+            If the request fails.
+
+        Returns
+        -------
+        roles : dict
+            Dictionary of current kitchen staff and their associated roles of the form::
+
+                {
+                    'Admin': ['admin@gmail.com'],
+                    'Developer': ['developer1@gmail.com', 'developer2@gmail.com'],
+                }
+        """
+        if settings:
+            return settings['kitchen']['kitchen-roles']
+        return self._get_settings()['kitchen']['kitchen-roles']
+
+    def _get_staff_set(self, settings: dict = None) -> set:
+        """
+        Retrieve the set of staff usernames assigned to this kitchen.
+
+        Parameters
+        ----------
+        settings : dict, optional
+            Dictionary representing kitchen_settings.json.
+
+        Raises
+        ------
+        HTTPError
+            If the request fails.
+
+        Returns
+        -------
+        roles : list
+            set of current kitchen staff usernames of the form::
+
+                {
+                    'admin@gmail.com',
+                    'developer1@gmail.com',
+                    'developer2@gmail.com',
+                }
+        """
+        if settings:
+            return set(settings['kitchen']['kitchen-staff'])
+        return set(self._get_settings()['kitchen']['kitchen-staff'])
+
+    def _ensure_admin(self, roles: dict = None, settings: dict = None) -> None:
+        """
+        Ensure the user in the provided client has Admin privileges in this kitchen. Otherwise,
+        raise a PermissionError.
+
+        Parameters
+        ----------
+        roles : dict, optional
+            Dictionary keyed by role and valued by list of associated usernames.
+        settings : dict, optional
+            Dictionary representing kitchen_settings.json.
+
+        Raises
+        ------
+        HTTPError
+            If the request fails.
+        PermissionError
+            If the client user is not an Admin in this kitchen.
+        """
+        if not roles:
+            roles = self._get_roles(settings=settings)
+
+        current_user = self._client._username
+        if current_user not in roles['Admin']:
+            raise PermissionError(f'Current user ({current_user}) not an Admin. Permission denied.')
+
+    def _ensure_disjoint(self, lists: list) -> bool:
+        """
+        Return True if all the provided lists (converted to sets) are disjoint, False otherwise.
+
+        Parameters
+        ----------
+        lists : list
+            List of lists. Each list will be converted to a set to ensure they are all disjoint.
+
+        Returns
+        -------
+        bool
+            True if provided lists are disjoint, False otherwise.
+        """
+        if len(lists) > 1:
+            merged_set = set(lists[0])
+            for l in lists[1:]:
+                if not merged_set.isdisjoint(set(l)):
+                    return False
+                merged_set = merged_set | set(l)
+        return True
+
     def get_alerts(self) -> dict:
         """
         Retrieve alerts set on this kitchen.
@@ -251,3 +357,179 @@ class Kitchen:
                 existing_alerts[k] = alert_emails
 
         self._update_settings(settings)
+
+    def get_staff(self) -> dict:
+        """
+        Retrieve the staff and their associated roles assigned to this kitchen.
+
+        Raises
+        ------
+        HTTPError
+            If the request fails.
+        PermissionError
+            If the current user is not an Admin
+
+        Returns
+        -------
+        staff : dict
+            Dictionary of current kitchen staff and their associated roles of the form::
+
+                {
+                    'Admin': ['admin@gmail.com'],
+                    'Developer': ['developer1@gmail.com', 'developer2@gmail.com'],
+                }
+        """
+        return self._get_roles()
+
+    def delete_staff(self, staff_to_delete: Union[set, list]) -> Response:
+        """
+        Delete the provided staff from this kitchen.
+
+        Parameters
+        ----------
+        staff_to_delete : set or list
+            List or set of usernames to delete from this kitchen's staff.
+
+        Raises
+        ------
+        HTTPError
+            If the request fails.
+        PermissionError
+            If the current user is not an Admin
+
+        Returns
+        -------
+        requests.Response
+            :class:`Response <Response>` object
+        """
+        if not isinstance(staff_to_delete, set):
+            staff_to_delete = set(staff_to_delete)
+
+        settings = self._get_settings()
+        self._ensure_admin(settings=settings)
+
+        # Remove staff from list
+        current_staff = self._get_staff_set(settings)
+        settings['kitchen']['kitchen-staff'] = list(current_staff - staff_to_delete)
+
+        # Remove staff from roles
+        for role, role_staff in self._get_roles(settings=settings).items():
+            settings['kitchen']['kitchen-roles'][role] = list(set(role_staff) - staff_to_delete)
+
+        return self._update_settings(settings)
+
+    def add_staff(self, staff_to_add: dict) -> Response:
+        """
+        Add the provided staff to this kitchen.
+
+        Parameters
+        ----------
+        staff_to_add : dict
+            Dictionary keyed by role and valued with list of users to add to that role in the form::
+
+                {
+                    'Admin': ['admin@gmail.com'],
+                    'Developer': ['developer1@gmail.com', 'developer2@gmail.com'],
+                }
+
+        Raises
+        ------
+        HTTPError
+            If the request fails.
+        PermissionError
+            If the current user is not an Admin
+        ValueError
+            If the provided staff are not new or unique across roles.
+
+        Returns
+        -------
+        requests.Response
+            :class:`Response <Response>` object
+        """
+        if not self._ensure_disjoint(list(staff_to_add.values())):
+            raise ValueError(f'Staff lists for each role must be unique.')
+
+        settings = self._get_settings()
+        self._ensure_admin(settings=settings)
+
+        # Add staff to list
+        current_staff = self._get_staff_set(settings)
+        new_staff = set(itertools.chain.from_iterable(staff_to_add.values()))
+
+        if not current_staff.isdisjoint(new_staff):
+            raise ValueError(
+                f'The following staff already exist in kitchen: {current_staff & new_staff}'
+            )
+
+        settings['kitchen']['kitchen-staff'] = list(current_staff | new_staff)
+
+        # Add staff to roles
+        for role, role_staff_to_add in staff_to_add.items():
+            cur_role_staff = settings['kitchen']['kitchen-roles'][role]
+            settings['kitchen']['kitchen-roles'][role] = list(
+                set(cur_role_staff) | set(role_staff_to_add)
+            )
+
+        return self._update_settings(settings)
+
+    def update_staff(self, staff_to_update: dict) -> Response:
+        """
+        Update roles for the provided staff to this kitchen.
+
+        Parameters
+        ----------
+        staff_to_update : dict
+            Dictionary keyed by role and valued with list of users to update to that role in the form::
+
+                {
+                    'Admin': ['admin@gmail.com'],
+                    'Developer': ['developer1@gmail.com', 'developer2@gmail.com'],
+                }
+
+        Raises
+        ------
+        HTTPError
+            If the request fails.
+        PermissionError
+            If the current user is not an Admin
+        ValueError
+            If the provided staff are not new or unique across roles.
+
+        Returns
+        -------
+        requests.Response
+            :class:`Response <Response>` object
+        """
+        if not self._ensure_disjoint(list(staff_to_update.values())):
+            raise ValueError(f'Staff lists for each role must be unique.')
+
+        settings = self._get_settings()
+        self._ensure_admin(settings=settings)
+
+        # Add staff to list
+        current_staff = self._get_staff_set(settings)
+        updated_staff = set(itertools.chain.from_iterable(staff_to_update.values()))
+
+        if not updated_staff.issubset(current_staff):
+            raise ValueError(
+                f'The following staff do not already exist in kitchen: {updated_staff - current_staff}'
+            )
+
+        # Update staff to roles
+
+        for role, role_staff_to_update in staff_to_update.items():
+            # Update role staff
+            cur_role_staff = settings['kitchen']['kitchen-roles'][role]
+            settings['kitchen']['kitchen-roles'][role] = list(
+                set(cur_role_staff) | set(role_staff_to_update)
+            )
+
+            # Remove updated staff from other roles
+            for other_role, cur_staff in settings['kitchen']['kitchen-roles'].items():
+                if other_role != role:
+                    cur_other_role_staff = settings['kitchen']['kitchen-roles'][other_role]
+                    settings['kitchen']['kitchen-roles'][other_role] = list(
+                        set(cur_other_role_staff) - set(role_staff_to_update)
+                    )
+
+        return self._update_settings(settings)
