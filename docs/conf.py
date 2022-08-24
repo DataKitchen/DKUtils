26520c61b93c7549ef619de9387645a7e384716e
DataKitchen <prod@datakitchen.io>
2022-08-24 21:28:09 +0000

    Bump version: 2.9.6 → 2.10.0

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2020, DataKitchen'
 author = 'DataKitchen'
 
 # The full version, including alpha/beta/rc tags
-release = u'2.9.6'
+release = u'2.10.0'
 
 # -- General configuration ---------------------------------------------------
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="DKUtils",
-    version="2.9.6",
+    version="2.10.0",
     author="DataKitchen",
     author_email="info@datakitchen.io",
     description="DataKitchen Utils Library",
